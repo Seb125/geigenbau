65214128cc6cfc694fe667640cccaf85f8c6752b
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-03-30 20:59:19 +0100

    myemail

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -14,7 +14,7 @@ function Test() {
     try {
       emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
       const response = await emailjs.send(import.meta.env.VITE_EMAIL_SERVICE_ID, import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-        {to_email:"schwarz.duscheleit@arcor.de",
+        {to_email:"schwarz.duscheleit@hotmail.de",
         subject: "Email von " + mail,
       message: message});
       console.log(response);
