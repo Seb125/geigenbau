007b441037269ae894f96e0843d8c8f2d5d7afb3
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-06-12 11:14:47 +0200

    eemails

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -19,7 +19,7 @@ function Test() {
     e.preventDefault();
     try {
       setLoading(true);
-      if(honeypot !== false) {
+      if(honeypot === false) {
       emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
       const response = await emailjs.send(
         import.meta.env.VITE_EMAIL_SERVICE_ID,
@@ -51,7 +51,7 @@ function Test() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName("needs-validation");
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
+        var validation = Array.prototype.forEach.call(forms, function (form) {
           form.addEventListener(
             "submit",
             function (event) {
